84c28824cea2705db8b5f5d81d42eaf8dccb5fdf
Zara <zaratam12@gmail.com>
2020-03-27 20:34:19 +0000

    WIP add search

--- project_1/wiki/encyclopedia/views.py
+++ project_1/wiki/encyclopedia/views.py
@@ -4,9 +4,21 @@ from . import util
 
 
 def index(request):
+
+    entries = util.list_entries()
+    entries_lowercase = [entry.lower() for entry in entries]
+
+    search_query = request.GET.get("q", "").lower()
+
+    if search_query and search_query in entries_lowercase:
+        entry_index = entries_lowercase.index(search_query)
+        title = entries[entry_index]
+        return entry(request, title)
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 
 def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
