78592cdea437cd5fa0ea36609110eff9999509cc
Zara <zaratam12@gmail.com>
2020-03-27 21:55:25 +0000

    WIP update search

--- project_1/wiki/encyclopedia/views.py
+++ project_1/wiki/encyclopedia/views.py
@@ -8,12 +8,24 @@ def index(request):
     entries = util.list_entries()
     entries_lowercase = [entry.lower() for entry in entries]
 
+    matched_entries = []
+
     search_query = request.GET.get("q", "").lower()
 
     if search_query and search_query in entries_lowercase:
         entry_index = entries_lowercase.index(search_query)
         title = entries[entry_index]
         return entry(request, title)
+
+    else:
+        for e in entries_lowercase:
+            if search_query in e:
+                entry_index = entries_lowercase.index(e)
+                title = entries[entry_index]
+                matched_entries.append(title)
+        return render(request, "encyclopedia/search.html", {
+            "matched_entries": matched_entries
+        })
 
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
